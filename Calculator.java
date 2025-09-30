e18b6baa548d85a2b1c781a0c0b403bbcfefa07c
Ashwini <ashwinidahifale2@gmail.com>
2025-09-30 20:01:18 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    // Method for addition
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    // Method for subtraction
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    // Method for multiplication
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    // Method for division
+    public static
